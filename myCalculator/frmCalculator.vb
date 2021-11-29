0c5035ed848740fde750b2d5fbf903407a4d5e0e
amsyafiiq <85499021+amsyafiiq@users.noreply.github.com>
2021-11-29 17:54:29 +0800

    Clean the code

--- myCalculator/frmCalculator.vb
+++ myCalculator/frmCalculator.vb
@@ -5,37 +5,35 @@
     Private value As Decimal
     Private operationBefore As String = ""
 
-    ' Handle button click event for numeric buttons
+    ' Button click event for numeric buttons
     Private Sub btn_Click(sender As Object, e As EventArgs) Handles btn0.Click, btn1.Click, btn2.Click, btn3.Click,
             btn4.Click, btn5.Click, btn6.Click, btn7.Click, btn8.Click, btn9.Click
 
         If operation = "=" Then btnClear_Click(sender, e)
         If operationBefore = "=" Then txtNumBox.Clear()
 
-        If txtNumBox.TextLength < 13 Then
-            Select Case DirectCast(sender, Button).Name
-                Case "btn0"
-                    txtNumBox.Text = txtNumBox.Text & 0
-                Case "btn1"
-                    txtNumBox.Text = txtNumBox.Text & 1
-                Case "btn2"
-                    txtNumBox.Text = txtNumBox.Text & 2
-                Case "btn3"
-                    txtNumBox.Text = txtNumBox.Text & 3
-                Case "btn4"
-                    txtNumBox.Text = txtNumBox.Text & 4
-                Case "btn5"
-                    txtNumBox.Text = txtNumBox.Text & 5
-                Case "btn6"
-                    txtNumBox.Text = txtNumBox.Text & 6
-                Case "btn7"
-                    txtNumBox.Text = txtNumBox.Text & 7
-                Case "btn8"
-                    txtNumBox.Text = txtNumBox.Text & 8
-                Case "btn9"
-                    txtNumBox.Text = txtNumBox.Text & 9
-            End Select
-        End If
+        Select Case DirectCast(sender, Button).Name
+            Case "btn0"
+                numDisplay(0)
+            Case "btn1"
+                numDisplay(1)
+            Case "btn2"
+                numDisplay(2)
+            Case "btn3"
+                numDisplay(3)
+            Case "btn4"
+                numDisplay(4)
+            Case "btn5"
+                numDisplay(5)
+            Case "btn6"
+                numDisplay(6)
+            Case "btn7"
+                numDisplay(7)
+            Case "btn8"
+                numDisplay(8)
+            Case "btn9"
+                numDisplay(9)
+        End Select
 
         operationBefore = ""
     End Sub
@@ -50,72 +48,19 @@
 
     ' Arithmethic calculation button
     Private Sub btnPlus_Click(sender As Object, e As EventArgs) Handles btnPlus.Click
-        value = Val(number)
-        'If operationBefore = "=" Then txtNumBox.Clear()
-
-        If number = "" Then
-            number = Val(txtNumBox.Text)
-            txtNumBox.Clear()
-            operation = "+"
-        Else
-            calc()
-            If operation <> "=" Then display() Else txtNumBox.Clear()
-            number = value
-            operation = "+"
-            operationBefore = "="
-        End If
+        operatorValidate("+")
     End Sub
 
     Private Sub btnMinus_Click(sender As Object, e As EventArgs) Handles btnMinus.Click
-        value = Val(number)
-        If operation = "=" Then txtNumBox.Clear()
-
-        If number = "" Then
-            number = Val(txtNumBox.Text)
-            txtNumBox.Clear()
-            operation = "-"
-        Else
-            calc()
-            If operation <> "=" Then display() Else txtNumBox.Clear()
-            number = value
-            operation = "-"
-            operationBefore = "="
-        End If
+        operatorValidate("-")
     End Sub
 
     Private Sub btnMultiply_Click(sender As Object, e As EventArgs) Handles btnMultiply.Click
-        value = Val(number)
-        If operation = "=" Then txtNumBox.Clear()
-
-        If number = "" Then
-            number = Val(txtNumBox.Text)
-            txtNumBox.Clear()
-            operation = "*"
-        Else
-            calc()
-            If operation <> "=" Then display() Else txtNumBox.Clear()
-            number = value
-            operation = "*"
-            operationBefore = "="
-        End If
-
+        operatorValidate("*")
     End Sub
 
     Private Sub btnDivide_Click(sender As Object, e As EventArgs) Handles btnDivide.Click
-        value = Val(number)
-        If operation = "=" Then txtNumBox.Clear()
-
-        If number = "" Then
-            number = Val(txtNumBox.Text)
-            txtNumBox.Clear()
-            operation = "/"
-        Else
-            calc()
-            If operation <> "=" Then display() Else txtNumBox.Clear()
-            number = value
-            operation = "/"
-            operationBefore = "="
-        End If
+        operatorValidate("/")
     End Sub
 
     ' Equal button
@@ -125,17 +70,19 @@
         If operationBefore <> "" And operation = "=" Then
             operation = operationBefore
             calc()
-            display()
+            displayAnswer()
             operation = "="
         Else
             calc()
-            display()
+            displayAnswer()
             number = txtNumBox.Text
             operationBefore = operation
             operation = "="
         End If
     End Sub
 
+
+    ' Display and Calculation Subroutine
     ' Calculation
     Sub calc()
         value = Val(number)
@@ -143,28 +90,55 @@
             Select Case operation
                 Case "+"
                     value += Val(txtNumBox.Text)
-                    number = value
                 Case "-"
                     value -= Val(txtNumBox.Text)
-                    number = value
                 Case "*"
                     value *= Val(txtNumBox.Text)
-                    number = value
                 Case "/"
                     value /= Val(txtNumBox.Text)
-                    number = value
             End Select
+            number = value
+
         Catch ex As Exception
             MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning)
         End Try
     End Sub
 
-    ' Output validation
-    Sub display()
+    ' Answer output validation
+    Private Sub displayAnswer()
         If number.Length < 13 Then
             txtNumBox.Text = number
         Else
             txtNumBox.Text = number.Substring(0, 13)
+        End If
+    End Sub
+
+    ' Numeric button input validation
+    Private Sub numDisplay(num As Integer)
+        If txtNumBox.Text = "0" Then
+            txtNumBox.Text = String.Empty
+        End If
+
+        If txtNumBox.TextLength < 13 Then
+            txtNumBox.Text = txtNumBox.Text & num
+        End If
+    End Sub
+
+    ' Calculation button input validation
+    Private Sub operatorValidate(ByVal operators As String)
+        value = Val(number)
+        If operation = "=" Then txtNumBox.Clear()
+
+        If number = "" Then
+            number = Val(txtNumBox.Text)
+            txtNumBox.Clear()
+            operation = operators
+        Else
+            calc()
+            If operation <> "=" Then displayAnswer() Else txtNumBox.Clear()
+            number = value
+            operation = operators
+            operationBefore = "="
         End If
     End Sub
 End Class
